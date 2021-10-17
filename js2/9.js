f45d2cd6ccf5fa9f945c08d15749d94c3e3c617d
Seungwoo Jo <sjosjo1204@gmail.com>
2021-10-17 23:33:27 +0900

    Solve problem 9

--- js2/9.js
+++ js2/9.js
@@ -5,8 +5,16 @@
  */
 
 const solution = () => {
-  Array.prototype.cReduce = function () {
-    return 0
+  Array.prototype.cReduce = function (cb, initialValue) {
+    let rec = (idx, acc) => {
+      if (idx >= this.length)
+        return acc
+      let newAcc = cb(acc, this[idx], idx, this)
+      return rec(idx+1, newAcc)
+    }
+    if (initialValue === undefined)
+      return rec(1, this[0]) 
+    return rec(0, initialValue)
   }
 }
 
