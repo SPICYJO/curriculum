2732be8d4a8e10c8596482c4563d0a257f3605d4
Seungwoo Jo <sjosjo1204@gmail.com>
2021-11-06 19:34:13 +0900

    Solve problem 4

--- js3/4.js
+++ js3/4.js
@@ -6,8 +6,14 @@
  * @returns {boolean}
  */
 
-const solution = (arr, num) => {
-  return true
+const solution = (arr, num, i=0, memory={}) => {
+  if (i >= arr.length)
+    return false
+  
+  if (memory.hasOwnProperty(num-arr[i]))
+    return true
+  memory[arr[i]] = true
+  return solution(arr, num, i+1, memory)
 }
 
 module.exports = {
