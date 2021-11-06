b6013d204a6d019064f49305e87207501c823c79
Seungwoo Jo <sjosjo1204@gmail.com>
2021-11-06 21:24:19 +0900

    Solve problem 6

--- js3/6.js
+++ js3/6.js
@@ -4,8 +4,17 @@
  * @returns {array}
 */
 
-const solution = (arr) => {
-  return []
+const solution = (arr, i=0, result=[], pushed={}, seen={}) => {
+  if (i >= arr.length) {
+    return result
+  }
+  
+  if (seen.hasOwnProperty(arr[i]) && !pushed.hasOwnProperty(arr[i])) {
+    result.push(arr[i])
+    pushed[arr[i]] = true
+  }
+  seen[arr[i]] = true
+  return solution(arr, i+1, result, pushed, seen)
 }
 
 module.exports = {
