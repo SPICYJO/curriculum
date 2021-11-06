2e542f5125812113e0540a522a43e8b6ed958af0
Seungwoo Jo <sjosjo1204@gmail.com>
2021-11-06 19:10:40 +0900

    Solve problem 1

--- js3/1.js
+++ js3/1.js
@@ -4,8 +4,14 @@
  * @returns {array} arr
  */
 
-const solution = (arr, obj) => {
-  return []
+const solution = (arr, obj, i=0) => {
+  if (i >= arr.length)
+    return []
+  
+  let s = solution(arr, obj, i+1)
+  if (obj.hasOwnProperty(arr[i]))
+    s.unshift(obj[arr[i]])
+  return s
 }
 
 module.exports = {
