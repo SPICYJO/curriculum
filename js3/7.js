8c6a7299350e72cd128d131a8f119835cdb06db5
Seungwoo Jo <sjosjo1204@gmail.com>
2021-11-07 11:02:33 +0900

    Solve problem 7

--- js3/7.js
+++ js3/7.js
@@ -6,7 +6,10 @@
 
 const solution = () => {
   Object.prototype.map = function (cb) {
-    return []
+    return Object.keys(this).reduce((acc, key, i) => {
+      acc.push(cb(key, this[key], i))
+      return acc
+    }, [])
   }
 }
 
